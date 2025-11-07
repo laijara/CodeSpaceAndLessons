7a3b385a7d6b790b48dbcf20eb6d0cca19231604
Shamber <147848650+laijara@users.noreply.github.com>
2025-11-07 09:08:10 +0000

    Lessons

---
+++ VerSS/lesson3/calctypes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    printf("%zu", sizeof(char));
+    printf("\n");
+    printf("%zu", sizeof(short));
+    printf("\n");
+    printf("%zu", sizeof(int));
+    printf("\n");
+    printf("%zu", sizeof(long));
+    printf("\n");
+    printf("%zu", sizeof(float));
+    printf("\n");
+    printf("%zu", sizeof(double));
+    printf("\n");
+    return 0;
+}
