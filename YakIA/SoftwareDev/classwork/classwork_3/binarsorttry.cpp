f229ce1c271f8fe38cc624adf19bcb5abde929d7
Shamber <147848650+laijara@users.noreply.github.com>
2026-03-26 09:06:29 +0000

    .

--- YakIA/SoftwareDev/classwork/classwork_3/binarsorttry.cpp
+++ YakIA/SoftwareDev/classwork/classwork_3/binarsorttry.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
 
-void CompletionArray(int arr[]) {
-    for (int i = 0; i <= 100; i++) {
-        arr[i] = i;
+void CompletionArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        std::cin >> arr[i];
     }
 }
 
 int main() {
     setlocale(LC_ALL, "ru");
-    int arr[100], l = 1, r, m, inpp;
-    std::cout << "Введите число - ";
+    int arr[100], l = 1, r = 0, m = 0, inpp, y;
+    std::cout << "Введите количество элементов в массиве - ";
     std::cin >> inpp;
-    CompletionArray(arr);
+    CompletionArray(arr, inpp);
+    std::cout << "Введите искомое число - ";
+    std::cin >> y;
     while (l < r)
     {
-        if (arr[m]) {
+        if (arr[m] < y) {
             l = m + 1;
         }
         else {
             r = m;
+        }
+        if (arr[m] == y) {
+            std::cout << "Элемент - " << arr[m] << std::endl;
+            return 0;
         }
     }
     return 0;
