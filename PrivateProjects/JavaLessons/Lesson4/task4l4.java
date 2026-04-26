45fb3f5c7098daf51ddf1a5d332a84fe16cdd609
laijara <fireman0123212@gmail.com>
2026-04-27 00:59:41 +0300

    .

---
+++ PrivateProjects/JavaLessons/Lesson4/task4l4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class task4l4 {
+    private final static Scanner globalScanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int[] arrNums = { 1, 2, 3, 4, 5 };
+        int temp;
+        for (int i = arrNums.length - 1; i > 0; i--) {
+            if (i != arrNums.length - 1) {
+                temp = arrNums[i];
+                arrNums[i] = arrNums[i + 1];
+                arrNums[i + 1] = temp;
+            }
+            else {
+                arrNums[arrNums.length - 1] = 1;
+            }
+        }
+        for (int i = 0; i < arrNums.length - 1; i++) {
+            System.out.print(arrNums[i] + "   ");
+        }
+
+        System.out.print("\n");
+        globalScanner.nextLine();
+        globalScanner.close();
+    }
+}
