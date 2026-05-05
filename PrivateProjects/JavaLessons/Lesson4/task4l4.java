8bcf2a3b9d920f716e4b657f248e0fcdb95b3835
laijara <fireman0123212@gmail.com>
2026-05-06 00:22:01 +0300

    .

--- PrivateProjects/JavaLessons/Lesson4/task4l4.java
+++ PrivateProjects/JavaLessons/Lesson4/task4l4.java
@@ -5,17 +5,23 @@ class task4l4 {
     public static void main(String[] args) {
         int[] arrNums = { 1, 2, 3, 4, 5 };
         int temp;
+        for (int i = 0; i < arrNums.length; i++) {
+            System.out.print(arrNums[i] + "   ");
+        }
+        System.out.print("\n");
         for (int i = arrNums.length - 1; i > 0; i--) {
-            if (i != arrNums.length - 1) {
+            if (i == arrNums.length - 1) {
+                temp = arrNums[0];
+                arrNums[0] = arrNums[arrNums.length - 1];
+                arrNums[1] = temp;
+            }
+            else {
                 temp = arrNums[i];
                 arrNums[i] = arrNums[i + 1];
                 arrNums[i + 1] = temp;
             }
-            else {
-                arrNums[arrNums.length - 1] = 1;
-            }
         }
-        for (int i = 0; i < arrNums.length - 1; i++) {
+        for (int i = 0; i < arrNums.length; i++) {
             System.out.print(arrNums[i] + "   ");
         }
 
