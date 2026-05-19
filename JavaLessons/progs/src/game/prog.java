af6005a86b330f00b51401f4babf27cbbd106fb0
Shamber <147848650+laijara@users.noreply.github.com>
2026-05-19 13:56:27 +0300

    update pet game

--- JavaLessons/progs/src/game/prog.java
+++ JavaLessons/progs/src/game/prog.java
@@ -4,14 +4,65 @@ import java.util.Scanner;
 class prog{
     private static final Scanner globalScanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        prog.StartProgram();
+        System.out.println("\n\nНажмите Enter для продолжения!");
         globalScanner.nextLine();
         globalScanner.close();
+    }
+    private static void StartProgram(){
+        System.out.print("Введите имя питомца - ");
+        Pet dog = new Pet(globalScanner.nextLine(), 5, 5, 5);
+        int input;
+        while (true){
+            System.out.println("\n\n\n\n\n===================================");
+            System.out.println("Выберите пункт:");
+            System.out.println("0 - Выход");
+            System.out.println("1 - Узнать статус питомца");
+            System.out.println("2 - Покормить");
+            System.out.println("3 - Поиграть");
+            System.out.println("4 - Уложить спать");
+            System.out.print("Вводите - ");
+            input = globalScanner.nextInt();
+            globalScanner.nextLine();
+            switch (input){
+                case 0:
+                    return;
+                case 1:
+                    System.out.println("\n\n====================");
+                    System.out.println("Статус питомца:");
+                    dog.CheckStatus();
+                    globalScanner.nextLine();
+                    break;
+                case 2:
+                    System.out.println("\n\n====================");
+                    System.out.println("Вы покормили " + dog.getName() + "! Нажмите Enter для продолжения!");
+                    dog.feed();
+                    globalScanner.nextLine();
+                    break;
+                case 3:
+                    System.out.println("\n\n====================");
+                    System.out.println("Вы поиграли с " + dog.getName() + "! Нажмите Enter для продолжения!");
+                    dog.play();
+                    globalScanner.nextLine();
+                    break;
+                case 4:
+                    System.out.println("\n\n====================");
+                    System.out.println("Вы уложили " + dog.getName() + " спать! Нажмите Enter для продолжения!");
+                    dog.sleep();
+                    globalScanner.nextLine();
+                    break;
+                default:
+                    System.out.println("\n\n====================");
+                    System.out.println("Ошибка ввода! Нажмите Enter для продолжения!");
+                    globalScanner.nextLine();
+                    break;
+            }
+        }
     }
 }
 
 class Pet{
-    private String name;
+    private final String name;
     private int hunger;
     private int energy;
     private int happiness;
@@ -41,10 +92,13 @@ class Pet{
     //Поведение
     public void feed(){
         if(this.energy <= 0){
-            System.out.println("Питомец устал, дайте ему поспать!");
+            System.out.println(this.name + " устал, дайте ему поспать!");
         }
         else if(this.energy - 1 < 0){
-            System.out.println("Вы не можете это сделать, иначе питомец сильно устанет!");
+            System.out.println("Вы не можете это сделать, иначе " + this.name + " сильно устанет!");
+        }
+        else if(this.hunger >= 10) {
+            System.out.println(this.name + " не может есть! Он наелся");
         }
         else {
             this.hunger += 3;
@@ -53,10 +107,13 @@ class Pet{
     }
     public void play(){
         if(this.energy <= 0 || this.hunger <= 0){
-            System.out.println("Питомец устал, дайте ему поспать или поесть!");
+            System.out.println(this.name + " устал, дайте ему поспать или поесть!");
         }
         else if(this.energy - 2 < 0 || this.hunger - 2 < 0){
-            System.out.println("Вы не можете это сделать, иначе питомец изголодает или сильно устанет!");
+            System.out.println("Вы не можете это сделать, иначе " + this.name + " изголодает или сильно устанет!");
+        }
+        else if (this.happiness >= 10) {
+            System.out.println(this.name + " не может играть! Он наигрался!");
         }
         else {
             this.hunger -= 2;
@@ -66,10 +123,13 @@ class Pet{
     }
     public void sleep(){
         if(this.hunger <= 0){
-            System.out.println("Питомец устал, дайте ему поесть!");
+            System.out.println(this.name + " голоден, дайте ему поесть!");
         }
         else if(this.hunger - 1 < 0){
-            System.out.println("Вы не можете это сделать, иначе питомец изголодает!");
+            System.out.println("Вы не можете это сделать, иначе " + this.name + " изголодает!");
+        }
+        else if (this.energy >= 10) {
+            System.out.println(this.name + " не может играть! Он наигрался");
         }
         else {
             this.hunger -= 1;
@@ -79,9 +139,9 @@ class Pet{
 
     //Другое
     public void CheckStatus(){
-        System.out.println(getName());
-        System.out.println(getHunger());
-        System.out.println(getEnergy());
-        System.out.println(getHappiness());
+        System.out.println("Имя - " + getName());
+        System.out.println("Сытость - " + getHunger());
+        System.out.println("Энергия - " + getEnergy());
+        System.out.println("Счастье - " + getHappiness());
     }
 }
